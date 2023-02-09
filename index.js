9ebb5bdd8fe99a7ae5f8646dfa5e7bfa9aa371fb
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-02-09 15:11:49 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,127 +1,82 @@
 // Import stylesheets
 import './style.css';
-src = 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js';
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
-  authDomain: 'skull-candy-48fae.firebaseapp.com',
-  databaseURL:
-    'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'skull-candy-48fae',
-  storageBucket: 'skull-candy-48fae.appspot.com',
-  messagingSenderId: '757603856879',
-  appId: '1:757603856879:web:990480f0446a513a7d461a',
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-import { initializeApp } from 'firebase/app';
-
-import { getDatabase, ref, set, child, get } from 'firebase/app';
-const db = getDatabase();
+import { getfirestore, child, ref, set } from 'firebase/firestore';
 
 const name = document.getElementById('nameInp');
 const email = document.getElementById('emailInp');
 const username = document.getElementById('userInp');
-const password = document.getElementById('passInp');
-const submit = document.getElementById('sub_btn');
+const pass = document.getElementById('passInp');
+const submit = document.getElementById('submit');
 
-function validation() {
-  let nameregx = /^[a-zA-Z]+$/;
-  let username = /^[a-zA-Z)-9]{5,}$/;
-
-  if (nameregx.test(name.value)) {
-    alert('Error occured!');
-    return false;
-  }
-  if (username.test(username.value)) {
-    alert('Error in username');
-    return false;
-  }
+async function main() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
+    authDomain: 'skull-candy-48fae.firebaseapp.com',
+    databaseURL:
+      'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'skull-candy-48fae',
+    storageBucket: 'skull-candy-48fae.appspot.com',
+    messagingSenderId: '757603856879',
+    appId: '1:757603856879:web:990480f0446a513a7d461a',
+  };
+  const db = getfirestore(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  submit.addEventListener('click', RegisterUser);
   return true;
 }
 
-function registerUser() {
-  if (validation()) {
+main();
+
+function Validation() {
+  let nameregex = /^[a-zA-Z]+$/;
+  let emailregex = /^[a-zA-Z0-9]+@(gmail|yahoo|outlook)\.com$/;
+  let userregex = /^[a-zA-Z0-9]{5,}$/;
+
+  if (!nameregex.test(name.value)) {
+    alert('the name should only contains alphabets');
+    return false;
+  }
+  if (!emailregex.test(email.value)) {
+    alert('the email is not valid');
+    return false;
+  }
+  if (!userregex.test(username.value)) {
+    alert('the username is not valid');
+    return false;
+  }
+
+  return true;
+}
+
+function RegisterUser() {
+  if (Validation()) {
     return;
   }
   const dbRef = ref(db);
 
-  get(child(dbRef, 'Users/' + username.value)).then((snapshot) => {
-    if (snapshot.exists()) {
-      alert('Account already exists');
+  get(child(dbRef, 'UserList/' + username.value)).then((snapshot) => {
+    if (snapshot.exist()) {
+      alert('Account already Exist!');
     } else {
-      set(ref(db, 'Users/' + username.value), {
+      set(ref(db, 'UserList/' + username.value), {
         fullname: name.value,
         email: email.value,
         username: username.value,
-        password: password.value,
+        password: pass.value,
       })
         .then(() => {
-          alert('user added successfully');
+          alert('User added sucessfully !!');
         })
         .catch((error) => {
-          alert('error ' + error);
+          alert('Error: ' + error);
         });
     }
   });
 }
-
-submit.addEventListener('click', registerUser);
-import { getDatabase, ref, set, child, get } from 'firebase/app';
-const db = getDatabase();
-
-const name = document.getElementById('nameInp');
-const email = document.getElementById('emailInp');
-const username = document.getElementById('userInp');
-const password = document.getElementById('passInp');
-const submit = document.getElementById('sub_btn');
-
-function validation() {
-  let nameregx = /^[a-zA-Z]+$/;
-  let username = /^[a-zA-Z)-9]{5,}$/;
-
-  if (nameregx.test(name.value)) {
-    alert('Error occured!');
-    return false;
-  }
-
-  if (username.test(username.value)) {
-    alert('Error in username');
-    return false;
-  }
-  return true;
-}
-
-function registerUser() {
-  if (validation()) {
-    return;
-  }
-  const dbRef = ref(db);
-
-  get(child(dbRef, 'Users/' + username.value)).then((snapshot) => {
-    if (snapshot.exists()) {
-      alert('Account already exists');
-    } else {
-      set(ref(db, 'Users/' + username.value), {
-        fullname: name.value,
-        email: email.value,
-        username: username.value,
-        password: password.value,
-      })
-        .then(() => {
-          alert('user added successfully');
-        })
-        .catch((error) => {
-          alert('error ' + error);
-        });
-    }
-  });
-}
-
-submit.addEventListener('click', registerUser);
