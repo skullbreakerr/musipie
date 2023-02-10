f41c331b1808773db8aa0f7d063c4d25219d995d
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-02-10 12:22:01 +0530

    2 files modified

--- index.js
+++ index.js
@@ -14,41 +14,35 @@ const username = document.getElementById('userInp');
 const pass = document.getElementById('passInp');
 const submit = document.getElementById('sub_bt');
 
-async function main() {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
-    authDomain: 'skull-candy-48fae.firebaseapp.com',
-    databaseURL:
-      'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
-    projectId: 'skull-candy-48fae',
-    storageBucket: 'skull-candy-48fae.appspot.com',
-    messagingSenderId: '757603856879',
-    appId: '1:757603856879:web:990480f0446a513a7d461a',
-  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
+  authDomain: 'skull-candy-48fae.firebaseapp.com',
+  databaseURL:
+    'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'skull-candy-48fae',
+  storageBucket: 'skull-candy-48fae.appspot.com',
+  messagingSenderId: '757603856879',
+  appId: '1:757603856879:web:990480f0446a513a7d461a',
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  const db = getFirestore(app);
-  submit.addEventListener('click', RegisterUser(db));
-  return true;
-}
-
-main();
+const db = getFirestore(app);
 
 function Validation() {
   let nameregex = /^[a-zA-Z]+$/;
   let emailregex = /^[a-zA-Z0-9]+@(gmail|yahoo|outlook)\.com$/;
   let userregex = /^[a-zA-Z0-9]{5,}$/;
 
-  if (!nameregex.test(name.value)) {
+  if (!nameregex.test(name)) {
     alert('the name should only contains alphabets');
     return false;
   }
-  if (!emailregex.test(email.value)) {
+  if (!emailregex.test(email)) {
     alert('the email is not valid');
     return false;
   }
-  if (!userregex.test(username.value)) {
+  if (!userregex.test(username)) {
     alert('the username is not valid');
     return false;
   }
@@ -56,12 +50,12 @@ function Validation() {
   return true;
 }
 
-function RegisterUser(db) {
+function RegisterUser() {
   if (Validation()) {
     return;
   }
   try {
-    const docRef = await addDoc(collection(db, 'users'), {
+    const docRef = addDoc(collection(db, 'users'), {
       fullname: name.value,
       email: email.value,
       username: username.value,
@@ -71,4 +65,8 @@ function RegisterUser(db) {
   } catch (e) {
     console.error('Error adding document: ', e);
   }
+
+  return true;
 }
+
+submit.addEventListener('click', RegisterUser);
