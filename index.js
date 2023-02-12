97421b7dcccd9cf58ebd2fbd21afa6258dc604f4
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-02-12 13:30:41 +0530

    3 files modified

--- index.js
+++ index.js
@@ -14,21 +14,49 @@ const username = document.getElementById('userInp');
 const pass = document.getElementById('passInp');
 const submit = document.getElementById('sub_bt');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
-  authDomain: 'skull-candy-48fae.firebaseapp.com',
-  databaseURL:
-    'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'skull-candy-48fae',
-  storageBucket: 'skull-candy-48fae.appspot.com',
-  messagingSenderId: '757603856879',
-  appId: '1:757603856879:web:990480f0446a513a7d461a',
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let db;
+async function main() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyCt_I6Xc7vfo5YY8tx5PYbDdU0n404fm0c',
+    authDomain: 'skull-candy-48fae.firebaseapp.com',
+    databaseURL:
+      'https://skull-candy-48fae-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'skull-candy-48fae',
+    storageBucket: 'skull-candy-48fae.appspot.com',
+    messagingSenderId: '757603856879',
+    appId: '1:757603856879:web:990480f0446a513a7d461a',
+  };
+  // Initialize Firebase
+  initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+  db = getFirestore();
+  if (!Validation()) {
+    return;
+  }
+  submit.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    addDoc(collection(db, 'users/'), {
+      fullname: name.value,
+      email: email.value,
+      username: username.value,
+      password: pass.value,
+      timestamp: Date.now(),
+      name: auth.currentUser.displayName,
+      userId: auth.currentUser.uid,
+    });
+
+    name.value = '';
+    email.value = '';
+    username.value = '';
+    pass.value = '';
+
+    return false;
+  });
+  alert('Sign Up Sucessfully !!');
+}
+
+main();
 function Validation() {
   let nameregex = /^[a-zA-Z]+$/;
   let emailregex = /^[a-zA-Z0-9]+@(gmail|yahoo|outlook)\.com$/;
@@ -49,24 +77,3 @@ function Validation() {
 
   return true;
 }
-
-function RegisterUser() {
-  if (Validation()) {
-    return;
-  }
-  try {
-    const docRef = addDoc(collection(db, 'users'), {
-      fullname: name.value,
-      email: email.value,
-      username: username.value,
-      password: pass.value,
-    });
-    console.log('Document written with ID: ', docRef.id);
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-
-  return true;
-}
-
-submit.addEventListener('click', RegisterUser);
